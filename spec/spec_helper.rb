3fdec5d0b0b2d8317bc1a6d20fabb7bc4dbfd2a9
Marc Busqué <marc@lamarciana.com>
2017-03-20 09:42:12 +0100

    Install pry-byebug as debugger

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,7 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'front_matter_parser'
 require 'tempfile'
+require 'pry-byebug'
 Dir["#{File.expand_path('../support', __FILE__)}/*.rb"].each do |file|
   require file
 end
