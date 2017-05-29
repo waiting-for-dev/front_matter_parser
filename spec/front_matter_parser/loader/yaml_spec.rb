0122d5372f8a4929185f2a5ed4512af268eb140b
Marc Busqué <marc@lamarciana.com>
2017-05-29 11:47:41 +0200

    Fix test not reflecting a real scenario

--- spec/front_matter_parser/loader/yaml_spec.rb
+++ spec/front_matter_parser/loader/yaml_spec.rb
@@ -5,7 +5,7 @@ require 'spec_helper'
 describe FrontMatterParser::Loader::Yaml do
   describe '::call' do
     it 'loads using yaml parser' do
-      string = "{title: 'hello'}"
+      string = "title: 'hello'"
 
       expect(described_class.call(string)).to eq(
         'title' => 'hello'
